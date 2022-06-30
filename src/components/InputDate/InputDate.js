7b1601a2abb89a144dd7dfc8c4c5f18be639a2b5
Oscar <oscar@oxkr.es>
2022-06-30 20:47:38 +0200

    check start date before end

--- src/components/InputDate/InputDate.js
+++ src/components/InputDate/InputDate.js
@@ -6,8 +6,15 @@ export const InputDate = (props) => {
     const [error, setError] = useState('')
     const handleDates = (ev) => {
         const date = ev.currentTarget.value;
+        if (id === 'startDate' && moment(date).isAfter(dates.startDate)) {
+            setError('Start date must be before end date')
+            setDates({ startDate: null, endDate: null, updated: false })
+            return false;
+        } else {
+            setError('');
+        }
         if (id === 'endDate' && moment(date).isBefore(dates.startDate)) {
-            setError('End date must be after')
+            setError('End date must be after start date')
             setDates({ ...dates, endDate: null })
             return false;
         } else {
